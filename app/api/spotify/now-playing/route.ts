0b14b44d0fec4246445fe6771ed2c5fd7cc6ee51
Ayush Paul <paulayush@gmail.com>
2025-04-07 18:12:53 -0700

    remove bad code

--- app/api/spotify/now-playing/route.ts
+++
@@ -1,7 +0,0 @@
-import { getNowPlayingItem } from "@/lib/spotify"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  const response = await getNowPlayingItem()
-  return NextResponse.json(response)
-} 
